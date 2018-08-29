5599ba421ce6ea98084858bdf5fca5c26a6cd1d0
Tara Johnson <tara@madface.com>
2018-08-28 21:47:58 -0700

    Last location persists, added multiple map type views.

--- app/src/main/java/com/example/mapapp/MapsActivity.java
+++ app/src/main/java/com/example/mapapp/MapsActivity.java
@@ -1,10 +1,10 @@
 package com.example.mapapp;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -16,6 +16,13 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private int CURRENT_MAP_TYPE_INDEX = 1;
+    int[] MAP_TYPES = {
+            GoogleMap.MAP_TYPE_NORMAL,
+            GoogleMap.MAP_TYPE_HYBRID,
+            GoogleMap.MAP_TYPE_TERRAIN,
+            GoogleMap.MAP_TYPE_SATELLITE,
+    };
 
     private GoogleMap mMap;
 
@@ -31,19 +38,58 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
         ButterKnife.bind(this);
-        isSatellite = false;
+
+        if (mMap != null) {
+            loadPreferences();
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        savePreferences();
+    }
+
+    public void savePreferences() {
+        SharedPreferences prefs = getSharedPreferences(
+                "com.example.mapapp",
+                Context.MODE_PRIVATE
+        );
+
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("mapType", CURRENT_MAP_TYPE_INDEX);
+        editor.putFloat("lat", (float) mMap.getCameraPosition().target.latitude);
+        editor.putFloat("long", (float) mMap.getCameraPosition().target.longitude);
+        editor.putFloat("zoom", mMap.getCameraPosition().zoom);
+        editor.commit();
+    }
+
+    public void loadPreferences() {
+        SharedPreferences prefs = getSharedPreferences(
+                "com.example.mapapp",
+                Context.MODE_PRIVATE
+        );
+
+        CURRENT_MAP_TYPE_INDEX = prefs.getInt("mapType", GoogleMap.MAP_TYPE_NORMAL);
+
+        float lat = prefs.getFloat("lat", 41);
+        float longg = prefs.getFloat("long", -81);
+        float zoom = prefs.getFloat("zoom", 4);
+
+        setMapType();
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(zoom));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(lat, longg)));
     }
 
     @OnClick(R.id.toggleView)
     public void toggleSatellite() {
-        isSatellite = !isSatellite;
+        CURRENT_MAP_TYPE_INDEX++;
+        CURRENT_MAP_TYPE_INDEX = CURRENT_MAP_TYPE_INDEX % MAP_TYPES.length;
+        setMapType();
+    }
 
-        int mapType = GoogleMap.MAP_TYPE_NORMAL;
-        if (isSatellite) {
-            mapType = GoogleMap.MAP_TYPE_SATELLITE;
-        }
-
-        mMap.setMapType(mapType);
+    public void setMapType() {
+        mMap.setMapType(MAP_TYPES[CURRENT_MAP_TYPE_INDEX]);
     }
 
     @OnClick(R.id.zoomin)
@@ -64,9 +110,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @OnClick(R.id.goToSeattle)
     public void goToSeattle() {
-        LatLng seattle = new LatLng(47, -122);
-        mMap.addMarker(new MarkerOptions().position(seattle).title("Seattle"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(seattle));
+        LatLng cleveland = new LatLng(41, -81);
+        mMap.addMarker(new MarkerOptions().position(cleveland).title("Cleveland"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(cleveland));
 
     }
 
@@ -82,22 +128,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-
-
-    // key — Your application's API key. This key identifies your application. See Get a key for more information.
-    // input — The text input specifying which place to search for (for example, a name, address, or phone number).
-    // inputtype — The type of input. This can be one of either textquery or phonenumber. Phone numbers must be in international format (prefixed by a plus sign ("+"), followed by the country code, then the phone number itself). See E.164 ITU recommendation for more information.
-    @OnClick(R.id.search)
-    public void search() {
-        String url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json";
-        url += "?key=AIzaSyAVIXzH0XjY3kOEN0PQYEsmR6iWCOuCCIc";
-        url += "?key=pizza";
-        url += "?inputtype=textquery";
+        loadPreferences();
     }
 }
