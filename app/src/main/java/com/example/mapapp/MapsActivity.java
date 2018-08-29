4fd5d6ac3ee470084b23d1be3b0f3f46ad342684
Tara Johnson <tara@madface.com>
2018-08-28 19:13:19 -0700

    Added zoom in/out and map view toggle.

--- app/src/main/java/com/example/mapapp/MapsActivity.java
+++ app/src/main/java/com/example/mapapp/MapsActivity.java
@@ -3,6 +3,8 @@ package com.example.mapapp;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -17,6 +19,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
 
+    private boolean isSatellite;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +31,35 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
         ButterKnife.bind(this);
+        isSatellite = false;
+    }
+
+    @OnClick(R.id.toggleView)
+    public void toggleSatellite() {
+        isSatellite = !isSatellite;
+
+        int mapType = GoogleMap.MAP_TYPE_NORMAL;
+        if (isSatellite) {
+            mapType = GoogleMap.MAP_TYPE_SATELLITE;
+        }
+
+        mMap.setMapType(mapType);
+    }
+
+    @OnClick(R.id.zoomin)
+    public void zoomIn() {
+        float zoom = mMap.getCameraPosition().zoom;
+        setZoom(zoom + 1);
+    }
+
+    @OnClick(R.id.zoomout)
+    public void zoomOut() {
+        float zoom = mMap.getCameraPosition().zoom;
+        setZoom(zoom - 1);
+    }
+
+    public void setZoom(float zoom) {
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(zoom));
     }
 
     @OnClick(R.id.goToSeattle)
@@ -56,6 +89,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
+
+    // key — Your application's API key. This key identifies your application. See Get a key for more information.
+    // input — The text input specifying which place to search for (for example, a name, address, or phone number).
+    // inputtype — The type of input. This can be one of either textquery or phonenumber. Phone numbers must be in international format (prefixed by a plus sign ("+"), followed by the country code, then the phone number itself). See E.164 ITU recommendation for more information.
+    @OnClick(R.id.search)
     public void search() {
         String url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json";
         url += "?key=AIzaSyAVIXzH0XjY3kOEN0PQYEsmR6iWCOuCCIc";
